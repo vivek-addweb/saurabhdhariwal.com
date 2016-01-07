04a052d905aa70b821db99247d0d982dcb1325b9
saurabhd <saurabhdhariwal.com@gmail.com>
2016-01-07 17:38:27 +0530

    Updated Drupal core to 8.0.2

--- core/lib/Drupal.php
+++ core/lib/Drupal.php
@@ -63,8 +63,8 @@ use Drupal\Core\Url;
  *   class StuffDoingClass implements StuffDoingInterface {
  *     protected $lockBackend;
  *
- *     public function __construct(LockBackendInterface $lockBackend) {
- *       $this->lockBackend = $lockBackend;
+ *     public function __construct(LockBackendInterface $lock_backend) {
+ *       $this->lockBackend = $lock_backend;
  *     }
  *
  *     public function doStuff() {
@@ -81,7 +81,7 @@ class Drupal {
   /**
    * The current system version.
    */
-  const VERSION = '8.0.0-dev';
+  const VERSION = '8.0.2';
 
   /**
    * Core API compatibility.
