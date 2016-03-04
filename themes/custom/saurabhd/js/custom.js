70ec5e38e1ba2b712478fe5336d4f28520b1f433
denishaaddweb <denisha.addweb@gmail.com>
2016-03-04 13:20:39 +0530

    one image not slider issue

--- themes/custom/saurabhd/js/custom.js
+++ themes/custom/saurabhd/js/custom.js
@@ -1,1 +1,10 @@
-jQuery(document).ready(function(){jQuery(".bts-img-slider > .carousel > ul").addClass("carousel-inner").attr("role","listbox"),jQuery(".bts-img-slider > .carousel > ul > li").addClass("carousel-item"),jQuery(".bts-img-slider > .carousel > ul > li:first-child").addClass("active"),jQuery("#block-socialmedia ul li a").attr("target","_blank")});
+jQuery(document).ready(function(){jQuery(".bts-img-slider > .carousel > ul").addClass("carousel-inner").attr("role","listbox"),jQuery(".bts-img-slider > .carousel > ul > li").addClass("carousel-item"),jQuery(".bts-img-slider > .carousel > ul > li:first-child").addClass("active"),jQuery("#block-socialmedia ul li a").attr("target","_blank")
+	jQuery('ul.carousel-inner').each(function(){
+         	var len = jQuery(this).find('li').length;        	
+         	if(len < 2){
+         	jQuery(this).parent().find('.left.carousel-control').hide();
+         	jQuery(this).parent().find('.right.carousel-control').hide();
+         }
+    });
+
+});
